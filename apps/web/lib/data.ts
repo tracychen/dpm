554173475b2bffc15f5eb863c57347c05c687464
Tracy Chen <tracy.chen@avalabs.org>
2023-12-10 18:47:53 -0800

    fix(apps/web): populate dummy binary options
    Use these dummy values until we have a better defined schema.

--- apps/web/lib/data.ts
+++ apps/web/lib/data.ts
@@ -24,6 +24,13 @@ export const markets: Market[] = [
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
     type: MarketType.BINARY,
+    options: [
+        {
+          label: "[TODO] binary layout",
+          amountBet: 45000,
+          price: 0.5,
+        },
+    ]
   },
   {
     id: "2",
@@ -98,6 +105,15 @@ export const markets: Market[] = [
     tag: "US election",
     imageUrl: "https://picsum.photos/200?random=1",
     type: MarketType.BINARY,
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    options: [
+        {
+          label: "[TODO] binary layout",
+          amountBet: 45000,
+          price: 0.5,
+        },
+    ]
   },
   {
     id: "5",
